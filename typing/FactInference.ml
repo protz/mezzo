548a1aae202c9267f961210d09980b961f8dd4ea
protzenk <protzenk@0f7ccc15-3e29-4385-878f-320e1e960475>
2011-12-16 14:10:33 +0000

    Massive cleanup of FactInference.ml. Comments, better handling of De Bruijn indexes, more cases handled.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/hamlet@98 0f7ccc15-3e29-4385-878f-320e1e960475

--- typing/FactInference.ml
+++ typing/FactInference.ml
@@ -10,45 +10,116 @@ end
 
 module MD = ModeDeduction.Make(Mode)(TyCon)
 
-open MD
-
 (* This is what we return *)
-type facts = rule list
+type facts = MD.rule list
 
-(* When constraining type parameters, we know that:
-   - the data type τ is either marked as exclusive, and then we have no
-   constraints on its parameters
-   - or τ is marked as duplicable, and then some of its parameters are
-   duplicable; those are in the [bitmap] *)
-type state = Exclusive | Duplicable of bitmap | Affine
 
-(* This maps De Bruijn indices (inside a given type) to () if the parameter with
- * that index has to be duplicable, nothing otherwise. *)
-and bitmap = unit IndexMap.t
+(* ------------------------------------------------------------------------- *)
 
-(* Our internal working environment. *)
+(* Data structures for the fact inference algorithm. *)
+
+(* Our internal working environment. This is all maps, and their keys are all De
+ * Bruijn levels. When navigating inside a data type definition,
+ * - the first few levels point to toplevel data type definitions: this is the
+ *   [types] field;
+ * - the following levels are for the current type's parameters: this is the
+ *   [current] field – the first parameter of the type has the lowest level;
+ * - all levels after that are for binders we've crossed, for instance a ∀
+ *   quantifier: this is the [extra] field. *)
 type env = {
   (* Maps global *levels* to knowledge we have acquired so far: the variable
    * name, for debugging; its arity; the corresponding bitfield, if any. *)
   types: (Variable.name * int * state) IndexMap.t;
 
+  (* A bitfield, that maps De Bruijn levels to () if they have to be duplicable.
+   * The integer is for the arity of the data type we're currently inspecting. *)
+  current: int * bitmap;
+
+  (* This is currently not very useful, but we'll probably need it once we start
+   * allowing [foo: ∀(α::term).(duplicable α) => τ]. *)
+  extra: var IndexMap.t;
+
   (* The current De Bruijn level. *)
   level: index;
 }
 
+(* When constraining type parameters, we know that:
+   - the data type τ is either marked as exclusive or affine, and then we have
+   no constraints on its parameters
+   - or τ is marked as duplicable, and then some of its parameters are
+   duplicable; those are in the [bitmap] *)
+and state = Exclusive | Duplicable of bitmap | Affine
+
+(* This maps levels of the current type parameters to () if that index has to be
+ * duplicable, nothing otherwise. *)
+and bitmap = unit IndexMap.t
+
+(* The information we know about a variable bound inside a type, with ∀ for
+ * instance. *)
+and var = Variable.name
+
+
+(* ------------------------------------------------------------------------- *)
+
+(* Helper functions for working with environments and types. *)
+
+exception NotDuplicable of typ
+(* TEMPORARY this one will have to go eventually *)
+exception NotSupported of string
+
 (* [state env i] returns the associated state of the data type with De Bruijn
    *index* i, with respect to the current context env. *)
-let state { level; types } i =
+let state env i =
   try
-    let _, _, state = IndexMap.find (level - i) types in
+    let level = env.level - i in
+    let _, _, state = IndexMap.find level env.types in
     state
   with Not_found ->
-    (* TEMPORARY remove this debug information once everything works fine. *)
+    (* TEMPORARY remove this debug information once everything works fine.
+     * NB: we're currently rejecting type parameters that don't have kind ∗.
+     * Reaching this code would mean that the well-kindedness check failed. *)
     IndexMap.iter (fun k (name, arity, _state) ->
       Log.debug "%d: %a[%d]" k Printers.p_var name arity
-    ) types;
-    Log.debug "Wanted: %d, level = %d" i level;
+    ) env.types;
+    Log.debug "Wanted: %d, level = %d" i env.level;
     assert false
+
+(* [mark_duplicable env i] returns a new environment where the type parameter
+ * corresponding to De Bruijn index [i] has been marked as duplicable. *)
+let mark_duplicable env i =
+  let n_types = IndexMap.cardinal env.types in
+  let level = env.level - i in
+  let arity, bitmap = env.current in
+  if not (level >= n_types && level < n_types + arity) then begin
+    (* This means we're trying to mark as duplicable something that's not one of
+       the type parameters.
+       - It can't be a globally defined data type, because [data t = T { foo:
+         list; }] is not well-kinded.
+       - This means that it's one of the freshly introduced binders. *)
+    match IndexMap.find_opt level env.extra with
+    | Some _ ->
+        (* Print some meaningful error message here, saying that _ is quantified
+         * somehow and that we don't have the right hypothesis. *)
+        raise (NotDuplicable (TyVar i))
+    | None ->
+        (* If we get there then I'm seriously wrong. *)
+        assert false
+  end;
+  let bitmap = IndexMap.add level () bitmap in
+  { env with current = arity, bitmap }
+
+(* [ith_param_duplicable bitmap i] tells whether the i-th parameter of the type
+ * whose bitmap is [bitmap] should be duplicable. *)
+let ith_param_duplicable env bitmap i =
+  let level = IndexMap.cardinal env.types + i in
+  Option.unit_bool (IndexMap.find_opt level bitmap)
+
+(* [add_binder env name] returns the new environment after we've entered an
+ * extra binder whose name is [name] *)
+let add_binder env name =
+  let new_level = env.level + 1 in
+  let extra = IndexMap.add new_level name env.extra in
+  { env with level = new_level; extra }
 
 (* A small helper function. *)
 let flatten_tyapp t =
@@ -60,103 +131,143 @@ let flatten_tyapp t =
   in
   flatten_tyapp [] t
 
-exception NotDuplicable of typ
-(* TEMPORARY this one will have to go eventually *)
-exception NotSupported of string
 
-(* Perform a reverse-analysis of a type, and return a bitmap of all indexes that
- * must be marked as duplicable for the original type to be duplicable itself. *)
-let rev_duplicables
+(* ------------------------------------------------------------------------- *)
+
+(* Some debugging / printing functions. *)
+
+(* Give the global level of a type, get its state as a string. *)
+let string_of_state env i =
+  let _name, arity, state = IndexMap.find i env.types in
+  let n_cons = IndexMap.cardinal env.types in
+  match state with
+  | Duplicable bitmap ->
+      String.concat "" (Hml_List.make arity (fun i ->
+        match IndexMap.find_opt (n_cons + i) bitmap with
+        | Some () -> "x"
+        | None -> "-")
+      )
+  | Exclusive ->
+      "exclusive"
+  | Affine ->
+      "affine"
+
+(* For debugging purposes. *)
+let print_env (env: env) : unit =
+  let open Printers in
+  IndexMap.iter (fun index (name, arity, state) ->
+    Log.debug "%d: %a [%s]" index p_var name (string_of_state env index);
+    match state with
+    | Duplicable bitmap ->
+        Log.debug "  keys: %s" (String.concat ","
+          (List.map string_of_int (IndexMap.keys bitmap)))
+    | _ ->
+        ()
+  ) env.types
+
+
+(* ------------------------------------------------------------------------- *)
+
+(* The core of the algorithm. *)
+
+(* Perform a reverse-analysis of a type, and return an env with the current
+ * field set to a bitmap of all indexes that must be marked as duplicable for
+ * the original type to be duplicable itself. *)
+let rec rev_duplicables
     (env: env)
-    (t: typ)
-    (map: bitmap) : bitmap =
-  let rec rev_duplicables (map: bitmap) (t: typ) : bitmap =
-    match t with
-    | TyUnknown
-    | TyDynamic ->
-        map
+    (t: typ) : env =
+  match t with
+  | TyUnknown
+  | TyDynamic ->
+      env
 
-    | TyVar i ->
-        (* TEMPORARY this only works because we're not going through any binders
-         * inside the type definition. The treatment of De Bruijn indexes is
-         * definitely less than satisfactory... *)
-        Log.debug "Duplicable: %d" i;
-        IndexMap.add i () map
-        
-    (* Is this the correct behavior? We assume we only have ∀ followed by a
-     * function type, which is always duplicable... and that we don't run into ∃
-     * at all. *)
-    | TyForall _
-    | TyExists _ ->
-        map
+  | TyVar i ->
+      Log.debug "Duplicable: %d" i;
+      mark_duplicable env i
 
-    | TyApp _ as t ->
-      begin
-        let hd, ts = flatten_tyapp t in
-        let arity = List.length ts in
-        match hd with
-        | TyVar i ->
-          begin
-            match state env i with
-            | Exclusive | Affine ->
-                raise (NotDuplicable t)
-            | Duplicable hd_bitmap ->
-                (* For each argument of the type application, if [hd] says that
-                 * its i-th argument has to be duplicable, then:
-                 * - find all type variables present in the argument that have
-                 * to be duplicable for the argument to be duplicable as well
-                 * - and add them to the map of variables so far.
-                 * Beware, the index in the list is not the De Bruijn index! The
-                 * bitmap keys are De Bruijn indexes. *)
-                Hml_List.fold_lefti (fun i map ti ->
-                  let index = arity - i in
-                  match IndexMap.find_opt index hd_bitmap with
-                  | Some () ->
-                      rev_duplicables map ti
-                  | None ->
-                      map
-                ) map ts
-          end
-        | _ ->
-            raise (NotSupported "Sorry, we don't allow Fω yet, all parameters must have ground kinds.")
-      end
+  (* Is this the correct behavior? We assume we only have ∀ followed by a
+   * function type, which is always duplicable... and that we don't run into ∃
+   * at all. *)
+  | TyForall _
+  | TyExists _ ->
+      env
 
+  | TyApp _ as t ->
+    begin
+      let hd, ts = flatten_tyapp t in
+      match hd with
+      | TyVar i ->
+        begin
+          match state env i with
+          | Exclusive | Affine ->
+              raise (NotDuplicable t)
+          | Duplicable hd_bitmap ->
+              (* For each argument of the type application, if [hd] says that
+               * its i-th argument has to be duplicable, then:
+               * - find all type variables present in the argument that have
+               * to be duplicable for the argument to be duplicable as well
+               * - and add them to the map of variables so far.
+               * Beware, the index in the list is not the De Bruijn index! The
+               * bitmap keys are De Bruijn indexes. *)
+              Hml_List.fold_lefti (fun i env ti ->
+                if ith_param_duplicable env hd_bitmap i then
+                  rev_duplicables env ti
+                else
+                  env
+              ) env ts
+        end
+      | _ ->
+          raise (NotSupported "Sorry, we don't allow Fω yet, you can only apply types to a globally defined data type.")
+    end
 
-    | TyTuple ts ->
-        List.fold_left (fun map -> function
-          | TyTupleComponentValue t -> rev_duplicables map t
-          | TyTupleComponentPermission _ ->
-              (* TEMPORARY do something sensible in that case too *)
-              assert false
-        ) map ts
+  | TyTuple ts ->
+      List.fold_left (fun env -> function
+        | TyTupleComponentValue t ->
+            rev_duplicables env t
+        | TyTupleComponentPermission t ->
+            (* For a permission to be duplicable, the underlying type has to be
+             * duplicable too. *)
+            rev_duplicables env t
+      ) env ts
 
-    (* TEMPORARY: for now on, let's say we're not dealing with these. In the
-     * future, we'll have to match these to the correct data type, figure out what
-     * its parameters are, and then proceed just like with TyApp. *)
-    | TyConcreteUnfolded _ ->
-        assert false
+  (* TEMPORARY: for now on, let's say we're not dealing with these. In the
+   * future, we'll have to match these to the correct data type, figure out what
+   * its parameters are, and then proceed just like with TyApp. *)
+  | TyConcreteUnfolded _ ->
+      assert false
 
-    (* Singleton types are always duplicable. *)
-    | TySingleton _ ->
-        map
+  (* Singleton types are always duplicable. *)
+  | TySingleton _ ->
+      env
 
-    (* Arrows are always duplicable *)
-    | TyArrow _ ->
-        map
+  (* Arrows are always duplicable *)
+  | TyArrow _ ->
+      env
 
-    (* TEMPORARY This doesn't really count, does it? *)
-    | TyAnchoredPermission _
-    | TyEmpty
-    | TyStar _ ->
-        assert false
-  in
-  rev_duplicables map t
+  (* TEMPORARY This doesn't really count, does it? *)
+  | TyAnchoredPermission (x, t) ->
+      (* That shouldn't be an issue, since x is probably TySingleton *)
+      let env = rev_duplicables env x in
+      (* For x: τ to be duplicable, τ has to be duplicable as well *)
+      rev_duplicables env t
+  | TyEmpty ->
+      env
+  | TyStar (p, q) ->
+      (* For p ∗ q  to be duplicable, both p and q have to be duplicable. *)
+      let env = rev_duplicables env p in
+      rev_duplicables env q
 
 (* This creates the environment in its initial state, and transforms the
  * knowledge we have gathered on the data types into a form that's suitable
  * for our analysis. *)
 let create_and_populate_env (type_env: Types.env) : env =
   let n_cons = IndexMap.cardinal type_env.data_type_map in
+  let empty = {
+    types = IndexMap.empty;
+    current = 0, IndexMap.empty;
+    extra = IndexMap.empty;
+    level = n_cons;
+  } in
   let env = IndexMap.fold (fun i (flag, name, kind, _branches) env ->
     let _hd, kargs = SurfaceSyntax.flatten_kind kind in
     let arity = List.length kargs in
@@ -167,33 +278,8 @@ let create_and_populate_env (type_env: Types.env) : env =
     | SurfaceSyntax.Duplicable ->
         { env with types =
           IndexMap.add i (name, arity, (Duplicable IndexMap.empty)) env.types }
-  ) type_env.data_type_map { types = IndexMap.empty; level = n_cons } in
+  ) type_env.data_type_map empty in
   env
-
-(* Because inside the types, we're using De Bruijn indexes, the bitmaps are
- * reversed compared to the type declaration [data p1 p2 ...]. We flip them for
- * display purposes. *)
-let string_of_bitmap bitmap max =
-  String.concat "" (Hml_List.make max (fun i ->
-    match IndexMap.find_opt (max - i) bitmap with Some () -> "x" | None -> "-"))
-
-(* For debugging purposes. *)
-let print_bitmap bitmap max =
-  Log.debug "%s" (string_of_bitmap bitmap max)
-
-(* For debugging purposes. *)
-let print_env (env: env) : unit =
-  let open Printers in
-  IndexMap.iter (fun index (name, arity, state) ->
-    match state with
-    | Duplicable bitmap ->
-        let bits = string_of_bitmap bitmap arity in
-        Log.debug "%d: %a [%s]" index p_var name bits
-    | Exclusive ->
-        Log.debug "%d: %a [%s]" index p_var name "exclusive"
-    | Affine ->
-        Log.debug "%d: %a [%s]" index p_var name "affine"
-  ) env.types
 
 (* This performs one round of constraint propagation.
    - If the type is initially marked as Exclusive, it remains Exclusive.
@@ -215,36 +301,34 @@ let one_round (type_env: Types.env) (env: env) : env =
           IndexMap.find level type_env.data_type_map
         in
         Log.debug "Processing %a, arity %d" Printers.p_var name arity;
-        let base_level = env.level in
         (* The type is in De Bruijn, so keep track of how many binders we've
          * crossed to get inside the type. *)
-        let env = { env with level = base_level + arity } in
+        let sub_env = {
+          types = env.types;
+          current = arity, bitmap;
+          extra = IndexMap.empty;
+          level = env.level + arity;
+        } in
         let new_mode =
           try
             (* For each field, find out which parameters should be duplicable
              * for that field's type to be duplicable; merge them with the
              * current environment. *)
-            let bitmap = List.fold_left (fun map (_name, fields) ->
-                List.fold_left (fun map -> function
-                  | FieldValue (_name, typ) ->
-                      rev_duplicables env typ map
-                  | FieldPermission _typ ->
-                      (* TEMPORARY do something here too *)
-                      assert false
-                ) map fields
-              ) bitmap branches in
-            Duplicable bitmap
-          with NotDuplicable _ ->
+            let sub_env = List.fold_left (fun sub_env (_label, fields) ->
+                List.fold_left (fun sub_env -> function
+                  | FieldValue (_, typ)
+                  | FieldPermission typ ->
+                      rev_duplicables sub_env typ
+                ) sub_env fields
+              ) sub_env branches in
+            Duplicable (snd sub_env.current)
+          with NotDuplicable _t ->
             (* Some exception was raised: we hit a type that's [Exclusive] or
              * [Affine], so the whole type need to be affine... *)
             Affine
         in
         let new_state = name, arity, new_mode in
-        let env = {
-          types = IndexMap.add level new_state env.types;
-          level = base_level (* back at the base level *)
-        } in
-        env
+        { env with types = IndexMap.add level new_state env.types }
 
   ) env.types env
 
