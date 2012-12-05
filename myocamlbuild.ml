02f2ebf73f32d7071dbba7426175252814d6f3b0
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2012-12-05 12:06:02 +0100

    Finally! Remove ulex from the source tree.
    Ulex is packaged by OPAM, but so far, I had been unable to find the right
    ocamlbuild voodoo that would enable us to use the system-wide ulex. Apparently,
    after several failed attemps, I've finally found the right set of commands.
    Three cheers for OCamlbuild and its amazing documentation!

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -24,9 +24,6 @@ dispatch begin function
 | After_rules ->
     flag ["ocaml"; "compile"; "strict_sequence"] (A "-strict-sequence");
     flag ["ocaml"; "compile"; "my_warnings"] (S[A "-w"; A "@1..3@8..12@14..21@23..39"]);
-    flag ["ocaml"; "pp"; "use_ulex"] (S[A"camlp4o"; A"ulex/pa_ulex.cma"]);
     flag ["bin_annot"; "compile"; "ocaml"] (A "-bin-annot");
-    dep ["ocaml"; "ocamldep"; "use_ulex"] ["ulex/pa_ulex.cma"];
-    ocaml_lib ~tag_name:"use_ulex" "ulexing";
 | _ -> ()
 end;;
