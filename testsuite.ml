b9e1298f322491c4ddc3985963308b8da252443e
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2013-01-30 16:08:52 +0100

    Mark 018 as a known failure.

--- testsuite.ml
+++ testsuite.ml
@@ -764,7 +764,7 @@ let tests: (string * ((unit -> env) -> unit)) list = [
     (* This test should fail, and it does, but the current error message is bizarre *)
     (* I am marking it as a known failure for the moment *)
 
-  ("018.mz", simple_test Pass);
+  ("018.mz", simple_test ~known_failure:() Pass);
 
   ("vicious-cycle.mz", simple_test Pass);
 
