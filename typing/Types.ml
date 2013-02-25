6b6418b17e922bcd5605961c371e18432e6aceea
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2013-02-25 17:15:29 +0100

    Update TAGS.

--- typing/Types.ml
+++ typing/Types.ml
@@ -64,14 +64,14 @@ let fact_of_flag = function
 (* Various helpers for creating and destructuring [typ]s easily. *)
 
 (* Saves us the trouble of matching all the time. *)
-let (!!)        = function
+let ( !!  )       = function
   | TyOpen x -> x
   | _ as t -> Log.error "Not a TyOpen %a" !internal_ptype (empty_env, t);;
-let (!*)        = Lazy.force;;
-let (>>=)       = Option.bind;;
-let (|||) o1 o2 = if Option.is_some o1 then o1 else o2 ;;
-let (^=>) x y   = x && y || not x;;
-let (!!=) = function
+let ( !*  )       = Lazy.force;;
+let ( >>= )       = Option.bind;;
+let ( ||| ) o1 o2 = if Option.is_some o1 then o1 else o2 ;;
+let ( ^=> ) x y   = x && y || not x;;
+let ( !!= )       = function
   | TySingleton (TyOpen x) ->
       x
   | _ ->
