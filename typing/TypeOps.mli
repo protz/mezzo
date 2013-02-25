e10c92be6d3e29b53ef3b91a488c99a617caff3c
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2013-02-25 17:21:04 +0100

    Add missing [TypeOps] interface.

---
+++ typing/TypeOps.mli
@@ -0,0 +1,28 @@
+(*****************************************************************************)
+(*  Mezzo, a programming language based on permissions                       *)
+(*  Copyright (C) 2011, 2012 Jonathan Protzenko and François Pottier         *)
+(*                                                                           *)
+(*  This program is free software: you can redistribute it and/or modify     *)
+(*  it under the terms of the GNU General Public License as published by     *)
+(*  the Free Software Foundation, either version 3 of the License, or        *)
+(*  (at your option) any later version.                                      *)
+(*                                                                           *)
+(*  This program is distributed in the hope that it will be useful,          *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *)
+(*  GNU General Public License for more details.                             *)
+(*                                                                           *)
+(*  You should have received a copy of the GNU General Public License        *)
+(*  along with this program.  If not, see <http://www.gnu.org/licenses/>.    *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(** This module provides various operations for manipulating types. *)
+
+(** [collect t] syntactically separates [t] into a structural part and a
+ * permission part, i.e. it extracts all the permissions hidden inside [t] and
+ * returns them as a separate list. *)
+val collect : TypeCore.typ -> TypeCore.typ * TypeCore.typ list
+
+(** Mark all type variables reachable from a type. *)
+val mark_reachable : TypeCore.env -> TypeCore.typ -> TypeCore.env
