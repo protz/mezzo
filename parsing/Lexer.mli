c420a56f4cad28703ae3ad8def1ff0971e733b7c
François Pottier <francois.pottier@inria.fr>
2013-03-17 18:27:13 +0100

    Another cosmetic cleanup.

--- parsing/Lexer.mli
+++ parsing/Lexer.mli
@@ -19,13 +19,26 @@
 
 (** The lexer for Mezzo. *)
 
+(** The abstract type of lexer errors. *)
 type error
 
+(** A lexer error can be thrown by the function [token]. *)
 exception LexingError of error
 
+(** [init filename] must be called before using the lexer. *)
 val init: string -> unit
+
+(** [token] is the main entry point of the lexer. It produces one
+    token or raises [LexingError]. *)
 val token: Ulexing.lexbuf -> Grammar.token * Lexing.position * Lexing.position
 
+(** [print_error] displays a lexer error. *)
 val print_error: Buffer.t -> (Ulexing.lexbuf * error) -> unit
+
+(** [print_position] displays the lexer's current position. It can be used
+    to build a syntax error message. *)
 val print_position: Buffer.t -> Ulexing.lexbuf -> unit
+
+(** [p] displays the position that is passed as an argument. It can be used
+    to build (say) type error messages. *)
 val p: Buffer.t -> Lexing.position * Lexing.position -> unit
