4f4eb9685fe9816225e215b50bdef6f0de237794
Cyprien Mangin <cyprien.mangin@inria.fr>
2013-04-15 13:50:51 +0200

    New bug found when using abbreviations and interfaces. See [abbrev-4]

--- testsuite.ml
+++ testsuite.ml
@@ -928,6 +928,7 @@ let tests: (string * ((unit -> env) -> unit)) list = [
   ("abbrev-1.mz", pass);
   ("abbrev-2.mz", pass);
   ("abbrev-3.mz", pass);
+  ("abbrev-4.mz", pass_known_failure);
   ("existential-witness.mz", pass);
   ("residual.mz", pass_known_failure);
   ("quantifier-bug.mz", fail_known_failure);
