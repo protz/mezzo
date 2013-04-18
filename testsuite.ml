613da9226e2b9ad30396bc14085db30e13e84ee2
Cyprien Mangin <cyprien.mangin@inria.fr>
2013-04-18 14:54:32 +0200

    Found a new bug. See [exclusive-bug].

--- testsuite.ml
+++ testsuite.ml
@@ -933,6 +933,7 @@ let tests: (string * ((unit -> env) -> unit)) list = [
   ("fractional.mzi", pass); (* no implementation, for now *)
   ("vide.mz", pass);
   ("openvide.mz", pass);
+  ("exclusive-bug.mz", pass_known_failure);
 
   (* The tests below are intentionally not run as they cause the type-checker to
    * loop. We still want to list them as, eventually, we will want to fix them. *)
