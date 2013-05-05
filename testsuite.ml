1ec7513b0b4ca3f6aa65f1b73fe667652b495818
François Pottier <francois.pottier@inria.fr>
2013-05-05 10:27:01 +0200

    Added a new test that fails. Seems related to rich booleans and the encoding of &&.

--- testsuite.ml
+++ testsuite.ml
@@ -992,6 +992,7 @@ let tests: (string * ((unit -> env) -> unit)) list = [
   ("species.mz", pass);
   ("ghost00.mz", kfail);
   ("ifthen-bug.mz", pass);
+  ("magic-map.mz", pass_known_failure);
 
   (* The tests below are intentionally not run as they cause the type-checker to
    * loop. We still want to list them as, eventually, we will want to fix them. *)
