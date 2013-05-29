5bd609e13da99e9804ebdd613e4f59b8ca495e6f
Cyprien Mangin <cyprien.mangin@inria.fr>
2013-05-29 13:21:34 +0200

    Added a [why3env] directly into the functor [ArithChecker].

--- typing/Arith.ml
+++ typing/Arith.ml
@@ -82,17 +82,23 @@ exception Flexible
 
 (* This is a functor because we have to create a VarMap whose equality between
  * keys depends on a give [env]. *)
-module ArithChecker (E: ENV) = struct
-  module VarMap = VarMapModulo(E)
+
+module ArithChecker (E: sig val wenv: why3env val env: env end) = struct
+  
+  let wenv = E.wenv
+  let env = E.env
+  
+  module VarMap = VarMapModulo(struct let env = env end)
   type symbolMap = Term.vsymbol VarMap.t
 
-  (* [why3_check wenv hyps t] returns true if the conjunction of the terms in [hyps]
+  (* [why3_check hyps t] returns true if the conjunction of the terms in [hyps]
    * implies the term [t]. *)
-  let why3_check (wenv: why3env) (vars: symbolMap) (hyps: Term.term list) (t: Term.term): bool =
+  let why3_check (vars: symbolMap) (hyps: Term.term list) (t: Term.term): bool =
     (* Build the term. *)
     let hyp = List.fold_left Term.t_and Term.t_true hyps in
     let t = Term.t_implies hyp t in
     let t = Term.t_forall_close (List.map snd (VarMap.to_list vars)) [] t in
+    (*Format.printf "%a\n" Pretty.print_term t;*)
     (* Build the task. *)
     let task = mk_task wenv t in
     (* Call the prover. *)
@@ -104,48 +110,48 @@ module ArithChecker (E: ENV) = struct
     | Call_provers.Valid -> true
     | _ -> false
   
-  (* [tr_typ wenv vars t] translates the type [t] into a Why3 term. It takes
+  (* [tr_typ vars t] translates the type [t] into a Why3 term. It takes
    * into account the already translated variables in [vars]. *)
-  let rec tr_typ (wenv: why3env) (vars: symbolMap) (t: typ):
+  let rec tr_typ (vars: symbolMap) (t: typ):
     symbolMap * Term.term =
-    match modulo_flex E.env t with
+    match modulo_flex env t with
     | TyApp (TyOpen op, args) ->
         (* Translate the operands *)
-        let vars, l = tr_many wenv vars args in
+        let vars, l = tr_many vars args in
         (* Apply the right operator *)
-        vars, Term.t_app_infer (StringMap.find (get_symbol E.env op) wenv.symbols) l
+        vars, Term.t_app_infer (StringMap.find (get_symbol env op) wenv.symbols) l
     | TyLiteral i -> vars, Term.t_nat_const i
     | TyOpen var ->
-        if is_flexible E.env var then raise Flexible;
+        if is_flexible env var then raise Flexible;
         begin try
           (* We already saw this variable *)
           vars, Term.t_var (VarMap.find var vars)
         with Not_found ->
           (* New variable *)
-          let symbol = mk_vsymbol E.env var in 
+          let symbol = mk_vsymbol env var in 
           let new_map = VarMap.add var symbol vars in
           new_map, Term.t_var symbol
         end
     | _ -> assert false
   
-  (* [tr_many wenv vars l] is basically the folding of [l] with [tr_typ]. *)
-  and tr_many (wenv: why3env) (vars: symbolMap) (l: typ list):
+  (* [tr_many vars l] is basically the folding of [l] with [tr_typ]. *)
+  and tr_many  (vars: symbolMap) (l: typ list):
     symbolMap * Term.term list =
     match l with
     | [] -> vars, []
     | hd :: tl ->
         (* Update the [symbolMap] when calling [tr_typ] or [tr_many]. *)
-        let vars, t = tr_typ wenv vars hd in
-        let vars, tl = tr_many wenv vars tl in
+        let vars, t = tr_typ vars hd in
+        let vars, tl = tr_many vars tl in
         vars, t :: tl
   
   (* [implies wenv hyps consequence] wraps the above functions to verify that
    * [consequence] is implied by [hyps]. *)
-  let implies (wenv: why3env) (hyps: typ list) (consequence: typ): bool =
+  let implies (hyps: typ list) (consequence: typ): bool =
     let vars = VarMap.empty in
-    let vars, t = tr_typ wenv vars consequence in
-    let vars, hyps = tr_many wenv vars hyps in
-    why3_check wenv vars hyps t
+    let vars, t = tr_typ vars consequence in
+    let vars, hyps = tr_many vars hyps in
+    why3_check vars hyps t
 
 end
 
@@ -161,8 +167,8 @@ let check (env: env) (consequence: typ): bool =
   match wenv with
   | Some wenv -> (
     try
-      let module A = ArithChecker(struct let env = env end) in
-      A.implies wenv (fetch_arith env) consequence
+      let module A = ArithChecker(struct let wenv = wenv let env = env end) in
+      A.implies(fetch_arith env) consequence
     with Flexible -> false)
   | None ->
       (* Why3 isn't available, accept all arithmetic check. *)
