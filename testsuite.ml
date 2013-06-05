8c9e19a3ec9129deb9b3aa652b3a873d3430498b
Cyprien Mangin <cyprien.mangin@inria.fr>
2013-06-05 10:40:26 +0200

    Added another test that makes the type-checker fail.

--- testsuite.ml
+++ testsuite.ml
@@ -993,6 +993,7 @@ let tests: (string * ((unit -> env) -> unit)) list = [
   ("ifthen-bug.mz", pass);
   ("magic-map.mz", pass);
   ("exist-name-conflict.mz", fail_known_failure);
+  ("assert-point.mz", pass_known_failure);
 
   (* The tests below are intentionally not run as they cause the type-checker to
    * loop. We still want to list them as, eventually, we will want to fix them. *)
