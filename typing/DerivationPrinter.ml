507003a263e7c0253cce6eaf3889a3bab019ea77
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2013-06-07 11:36:39 +0200

    Tweak the derivation printer.

--- typing/DerivationPrinter.ml
+++ typing/DerivationPrinter.ml
@@ -39,8 +39,11 @@ let print_judgement env = function
   | JSubConstraint c ->
       words "satisfy" ^^^ print_constraint env c
   | JSubConstraints cs ->
-      let cs = List.map (print_constraint env) cs in
-      words "satisfy" ^^^ english_join cs
+      if List.length cs > 0 then
+        let cs = List.map (print_constraint env) cs in
+        words "satisfy" ^^^ english_join cs
+      else
+        empty
   | JEqual (t1, t2) ->
       words "prove equality:" ^^^
       print_type env t1 ^^^ equals ^^^ print_type env t2
