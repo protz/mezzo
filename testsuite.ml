827cead3a096a0b84f7ca2717c83fb26efc9925e
Cyprien Mangin <cyprien.mangin@inria.fr>
2013-06-11 11:54:10 +0200

    Still a bug with named returned values.
    See [named-return2].

--- testsuite.ml
+++ testsuite.ml
@@ -992,6 +992,7 @@ let tests: (string * ((unit -> env) -> unit)) list = [
   ("exist-name-conflict.mz", pass);
   ("assert-point.mz", pass);
   ("named-return.mz", pass);
+  ("named-return2.mz", pass_known_failure);
 
   (* The tests below are intentionally not run as they cause the type-checker to
    * loop. We still want to list them as, eventually, we will want to fix them. *)
