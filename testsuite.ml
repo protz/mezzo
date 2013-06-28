ec604dbd069a511f640789d8ca2727b4b82253b3
Armaël Guéneau <armael.gueneau@inria.fr>
2013-06-28 10:20:57 +0200

    Example of pack followed by an assert on the result that fails.
    See [pack-assert.mz]. I think this should pass, whereas it fails.

--- testsuite.ml
+++ testsuite.ml
@@ -1003,6 +1003,7 @@ let tests: (string * ((unit -> env) -> unit)) list = [
   ("landin.mz", pass);
   ("strange.mz", fail);
   ("covariantlock.mz", pass);
+  ("pack-assert.mz", pass_known_failure);
 
   (* The tests below are intentionally not run as they cause the type-checker to
    * loop. We still want to list them as, eventually, we will want to fix them. *)
