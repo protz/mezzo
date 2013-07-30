465435ff9495954aecaaa4fbaa3e396f3ddec3a7
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2013-07-30 16:33:43 +0200

    New bug.
    So apparently I can't do "if true then ..." twice in a row. Who wrote this
    type-checker?!!

--- testsuite.ml
+++ testsuite.ml
@@ -1022,6 +1022,7 @@ let tests: (string * ((unit -> env) -> unit)) list = [
   ("server.mz", pass);
   ("array-borrow-1.mz", pass);
   ("array-borrow-2.mz", fail);
+  ("booltrue.mz", pass_known_failure);
 
   (* The tests below are intentionally not run as they cause the type-checker to
    * loop. We still want to list them as, eventually, we will want to fix them. *)
