2380a4eb5d7466f3d7832a5df3625d77c86a2ae7
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2013-10-28 17:45:11 +0100

    Two soundness bugs.
    Yay...

--- testsuite.ml
+++ testsuite.ml
@@ -1040,6 +1040,8 @@ let tests: (string * ((unit -> env) -> unit)) list = [
   ("shortest.mz", pass);
   ("snapshot.mz", pass);
   ("adoptslib.mz", pass);
+  ("flexbug.mz", fail_known_failure);
+  ("flexbug2.mz", fail_known_failure);
 
   (* The tests below are intentionally not run as they cause the type-checker to
    * loop. We still want to list them as, eventually, we will want to fix them. *)
