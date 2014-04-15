4a20f56fb05ff883955c9ae41e7a0d33e88fd8a3
François Pottier <francois.pottier@inria.fr>
2014-04-15 10:21:46 +0200

    Added nesting02 (which works) and nesting03 (which causes an exception, List.nth).

--- testsuite.ml
+++ testsuite.ml
@@ -1045,6 +1045,8 @@ let tests: (string * ((unit -> env) -> unit)) list = [
   ("flexbug2.mz", fail);
   ("cons.mz", pass);
   ("nesting00.mz", pass);
+  ("nesting02.mz", pass);
+  ("nesting03.mz", pass);
 
   (* The tests below are intentionally not run as they cause the type-checker to
    * loop. We still want to list them as, eventually, we will want to fix them. *)
