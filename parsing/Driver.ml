7c7278e58ebf2fe3ce25997686a89a8eaff3e977
François Pottier <francois.pottier@inria.fr>
2015-01-19 19:05:48 +0100

    Modified [Driver] to use the experimental error-reporting facility.

--- parsing/Driver.ml
+++ parsing/Driver.ml
@@ -31,10 +31,323 @@ let chop_mz_or_mzi f =
     invalid_arg "chop_mz_or_mzi"
 ;;
 
+(* [print_symbol] *)
+
+module PS = struct
+
+  open Grammar.MenhirInterpreter
+
+  (* Painful. Plus, should update this list when the list of keywords
+     changes. TEMPORARY auto-generate the part that concerns keywords *)
+
+  let print_symbol symbol =
+    match symbol with
+    | X (T T_error) -> "error"
+    | X (T T_WITNESS) -> "witness"
+    | X (T T_WITH) -> "with"
+    | X (T T_WHILE) -> "while"
+    | X (T T_VALUE) -> "value"
+    | X (T T_VAL) -> "val"
+    | X (T T_UNKNOWN) -> "unknown"
+    | X (T T_UNDERSCORE) -> "_"
+    | X (T T_UIDENT) -> "<Data constructor>"
+    | X (T T_TYPE) -> "type"
+    | X (T T_TO) -> "to"
+    | X (T T_THEN) -> "then"
+    | X (T T_TAKING) -> "taking"
+    | X (T T_TAKE) -> "take"
+    | X (T T_TAGOF) -> "tag of"
+    | X (T T_STAR) -> "*"
+    | X (T T_SEMI) -> ";"
+    | X (T T_RPAREN) -> ")"
+    | X (T T_REC) -> "rec"
+    | X (T T_RBRACKET) -> "]"
+    | X (T T_RBRACE) -> "}"
+    | X (T T_PRESERVING) -> "preserving"
+    | X (T T_PLUS) -> "+"
+    | X (T T_PERM) -> "perm"
+    | X (T T_PACK) -> "pack"
+    | X (T T_OPPREFIX) -> "!"   (* a representative choice *)
+    | X (T T_OPINFIX4) -> "**"
+    | X (T T_OPINFIX3) -> "***" (* a representative choice *)
+    | X (T T_OPINFIX2) -> "++"  (* a representative choice *)
+    | X (T T_OPINFIX1) -> "@@"  (* a representative choice *)
+    | X (T T_OPINFIX0d) -> "$"
+    | X (T T_OPINFIX0c) -> "==" (* a representative choice *)
+    | X (T T_OPINFIX0b) -> "&&" (* a representative choice *)
+    | X (T T_OPINFIX0a) -> "||" (* a representative choice *)
+    | X (T T_OPEN) -> "open"
+    | X (T T_MUTABLE) -> "mutable"
+    | X (T T_MINUS) -> "-"
+    | X (T T_MATCH) -> "match"
+    | X (T T_LPAREN) -> "("
+    | X (T T_LIDENT) -> "<identifier>"
+    | X (T T_LET) -> "let"
+    | X (T T_LBRACKET) -> "["
+    | X (T T_LBRACE) -> "{"
+    | X (T T_LARROW) -> "<-"
+    | X (T T_INT) -> "42" (* a representative choice *)
+    | X (T T_IN) -> "in"
+    | X (T T_IF) -> "if"
+    | X (T T_GIVE) -> "give"
+    | X (T T_FUN) -> "fun"
+    | X (T T_FROM) -> "from"
+    | X (T T_FOR) -> "for"
+    | X (T T_FLEX) -> "flex"
+    | X (T T_FAIL) -> "fail"
+    | X (T T_FACT) -> "fact"
+    | X (T T_EXPLAIN) -> "explain"
+    | X (T T_EXCLUSIVE) -> "exclusive"
+    | X (T T_EQUAL) -> "="
+    | X (T T_EOF) -> "<eof>"
+    | X (T T_END) -> "end"
+    | X (T T_EMPTY) -> "empty"
+    | X (T T_ELSE) -> "else"
+    | X (T T_DYNAMIC) -> "dynamic"
+    | X (T T_DUPLICABLE) -> "duplicable"
+    | X (T T_DOWNTO) -> "downto"
+    | X (T T_DOT) -> "."
+    | X (T T_DO) -> "do"
+    | X (T T_DBLARROW) -> "=>"
+    | X (T T_DATA) -> "data"
+    | X (T T_CONSUMES) -> "consumes"
+    | X (T T_COMMA) -> ","
+    | X (T T_COLONEQUAL) -> ":="
+    | X (T T_COLONCOLON) -> "::"
+    | X (T T_COLON) -> ":"
+    | X (T T_BUILTIN) -> "builtin"
+    | X (T T_BELOW) -> "below"
+    | X (T T_BEGIN) -> "begin"
+    | X (T T_BAR) -> "|"
+    | X (T T_AT) -> "@"
+    | X (T T_ASSERT) -> "assert"
+    | X (T T_AS) -> "as"
+    | X (T T_ARROW) -> "->"
+    | X (T T_AND) -> "and"
+    | X (T T_ALIAS) -> "alias"
+    | X (T T_ADOPTS) -> "adopts"
+    | X (T T_ABSTRACT) -> "abstract"
+    | X (T T_ABOVE) -> "above"
+    | X (N N_warn_error_list) -> assert false
+    | X (N N_warn_error) -> assert false
+    | X (N N_variance) -> "<variance>"
+    | X (N N_variable) -> "<variable>"
+    | X (N N_value_declaration) -> "<value declaration>"
+    | X (N N_type_parameters) -> "<type parameters>"
+    | X (N N_type_binding) -> "<type binding>"
+    | X (N N_type_application_component) -> "<type application component>"
+    | X (N N_separated_or_terminated_list_SEMI_data_field_pattern_) -> "<field patterns>"
+    | X (N N_separated_or_terminated_list_SEMI_data_field_expression_) -> "<field expressions>"
+    | X (N N_separated_or_terminated_list_SEMI_data_field_def_) -> "<field definitions>"
+    | X (N N_separated_or_terminated_list_COMMA_type_binding_) -> "<type bindings>"
+    | X (N N_separated_or_preceded_list_BAR_match_branch_) -> "<branches>"
+    | X (N N_separated_or_preceded_list_BAR_data_type_def_branch_) -> "<data definitions>"
+    | X (N N_separated_nonempty_list_SEMI_data_field_pattern_) -> "<field patterns(1)>"
+    | X (N N_separated_nonempty_list_SEMI_data_field_expression_) -> "<field expressions(1)>"
+    | X (N N_separated_nonempty_list_SEMI_data_field_def_) -> "<field definitions(1)>"
+    | X (N N_separated_nonempty_list_DBLARROW_mode_constraint_) -> "<mode constraints(1)>"
+    | X (N N_separated_nonempty_list_COMMA_variable_) -> "<variables(1)>"
+    | X (N N_separated_nonempty_list_COMMA_type_binding_) -> "type bindings(1)>"
+    | X (N N_separated_nonempty_list_COMMA_type_application_component_) -> "<type application components(1)>"
+    | X (N N_separated_nonempty_list_COMMA_consumes_type_) -> "<types(consumes/1)>"
+    | X (N N_separated_nonempty_list_COMMA_atomic_pattern_) -> "<atomic patterns(1)>"
+    | X (N N_separated_nonempty_list_COMMA_algebraic_expression_) -> "<expressions(algebraic/1)>"
+    | X (N N_separated_nonempty_list_BAR_match_branch_) -> "<branches(1)>"
+    | X (N N_separated_nonempty_list_BAR_data_type_def_branch_) -> "<data definitions(1)>"
+    | X (N N_separated_nonempty_list_AND_definition_) -> "<definitions(1)>"
+    | X (N N_separated_nonempty_list_AND_concrete_data_type_def_) -> "<data type definitions(1)>"
+    | X (N N_rec_flag) -> "<rec?>"
+    | X (N N_reasonable_expression) -> "<expression(reasonable)>"
+    | X (N N_raw_very_loose_type) -> "<type(very loose)>"
+    | X (N N_raw_tuple_or_raw_reasonable_expression_) -> "<tuple|expression(reasonable)>"
+    | X (N N_raw_tuple_or_raw_fragile_expression_) -> "<tuple|expression(fragile)>"
+    | X (N N_raw_tight_type) -> "<type(tight)>"
+    | X (N N_raw_tight_expression) -> "<expression(tight)>"
+    | X (N N_raw_reasonable_expression) -> "<expression(reasonable)>"
+    | X (N N_raw_parenthetic_type) -> "<type(parenthetic)>"
+    | X (N N_raw_normal_type_no_adopts) -> "<type(normal)>"
+    | X (N N_raw_normal_type) -> "<type(normal)>" (* putting under the rug the distinction with the previous case *)
+    | X (N N_raw_normal_pattern) -> "<pattern(normal)>"
+    | X (N N_raw_loose_type) -> "<type(loose)>"
+    | X (N N_raw_loose_pattern) -> "<pattern(loose)>"
+    | X (N N_raw_fragile_expression) -> "<expression(fragile)>"
+    | X (N N_raw_fat_type) -> "<type(fat)>"
+    | X (N N_raw_consumes_type) -> "<type(consumes)>"
+    | X (N N_raw_atomic_type) -> "<type(atomic)>"
+    | X (N N_raw_atomic_pattern) -> "<pattern(atomic)>"
+    | X (N N_raw_atomic_expression) -> "<expression(atomic)>"
+    | X (N N_raw_application_expression) -> "<expression(application)>"
+    | X (N N_raw_algebraic_expression) -> "<expression(algebraic)>"
+    | X (N N_range) -> assert false
+    | X (N N_parenthesized_tuple_components) -> "<expressions(algebraic*.fragile)>"
+    | X (N N_optional_preserving) -> "<preserving?>"
+    | X (N N_option_preceded_ADOPTS_arbitrary_type__) -> "<adopts clause?>"
+    | X (N N_nonempty_list_warn_error_) -> assert false
+    | X (N N_nonempty_list_terminated_type_binding_COMMA__) -> "type bindings(1)>"
+      (* TEMPORARY a simpler definition of separated_or_terminated_list would be good!
+         same remark for the optional bar in match constructs and data type defs. *)
+      (* TEMPORARY also think about left-recursive lists *)
+    | X (N N_nonempty_list_terminated_data_field_pattern_SEMI__) -> "<field patterns(1)>"
+    | X (N N_nonempty_list_terminated_data_field_expression_SEMI__) -> "<field expressions(1)>"
+    | X (N N_nonempty_list_terminated_data_field_def_SEMI__) -> "<field definitions(1)>"
+    | X (N N_nonempty_list_preceded_BAR_match_branch__) -> "<branches(1)>"
+    | X (N N_nonempty_list_preceded_BAR_data_type_def_branch__) -> "<data definitions(1)>"
+    | X (N N_mode) -> "<mode>"
+    | X (N N_maybe_qualified_variable_) -> "<qualified? variable>"
+    | X (N N_maybe_qualified_datacon_) -> "<qualified? data constructor>"
+    | X (N N_loption_type_parameters_) -> "<type parameters?>"
+    | X (N N_loption_separated_nonempty_list_AND_definition__) -> "<definitions>"
+    | X (N N_list_terminated_mode_constraint_DBLARROW__) -> "<mode constraints>" (* putting under the rug the distinction with another symbol *)
+    | X (N N_list_interface_item_) -> "<interface items>"
+    | X (N N_list_implementation_item_) -> "<implementation items>"
+    | X (N N_list_fact_) -> "<facts>"
+    | X (N N_list_atomic_type_binding_with_variance_) -> "<atomic type bindings with variance>"
+    | X (N N_interface_item) -> "<interface item>"
+    | X (N N_interface) -> "<interface>"
+    | X (N N_implementation_item) -> "<implementation item>"
+    | X (N N_implementation) -> "<implementation>"
+    | X (N N_generic_datacon_application_separated_or_terminated_list_SEMI_data_field_pattern__) -> "<data constructor pattern>"
+    | X (N N_generic_datacon_application_separated_or_terminated_list_SEMI_data_field_expression__) -> "<data constructor expression>"
+    | X (N N_generic_datacon_application_data_type_def_branch_content_) -> "<data constructor definition>"
+    | X (N N_flag) -> assert false
+    | X (N N_fact) -> "<fact>"
+    | X (N N_explain) -> "<explain?>"
+    | X (N N_existential_quantifiers) -> "<existential quantifiers>"
+    | X (N N_direction) -> "<direction>"
+    | X (N N_definition_group) -> "<definition group>"
+    | X (N N_definition) -> "<definition>"
+    | X (N N_data_type_def_branch_content) -> "<data constructor parameters>" (* not great *)
+    | X (N N_data_field_pattern) -> "<field pattern>"
+    | X (N N_data_field_expression) -> "<field expression>"
+    | X (N N_data_field_def) -> "<field definition>"
+    | X (N N_concrete_data_type_def) -> "<data type definition>"
+    | X (N N_atomic_type_binding_with_variance) -> "<atomic type binding with variance>"
+    | X (N N_atomic_type_binding) -> "<atomic type binding>"
+    | X (N N_atomic_kind) -> "<kind>"
+    | X (N N_anonymous_function) -> "<function>"
+    | X (N N_abstract_data_type_def) -> "<abstract type definition>"
+    | X (N N_abbreviation_def) -> "<alias definition>"
+
+end
+
+(* [terminal2token] *)
+
+module TT = struct
+
+  open Grammar
+  open Grammar.MenhirInterpreter
+
+  let terminal2token (type a) (t : a terminal) : token =
+    match t with
+    | T_error ->
+        assert false
+    | T_WITNESS -> WITNESS
+    | T_WITH -> WITH
+    | T_WHILE -> WHILE
+    | T_VALUE -> VALUE
+    | T_VAL -> VAL
+    | T_UNKNOWN -> UNKNOWN
+    | T_UNDERSCORE -> UNDERSCORE
+    | T_UIDENT -> UIDENT ""
+    | T_TYPE -> TYPE
+    | T_TO -> TO
+    | T_THEN -> THEN
+    | T_TAKING -> TAKING
+    | T_TAKE -> TAKE
+    | T_TAGOF -> TAGOF
+    | T_STAR -> STAR ""
+    | T_SEMI -> SEMI
+    | T_RPAREN -> RPAREN
+    | T_REC -> REC
+    | T_RBRACKET -> RBRACKET
+    | T_RBRACE -> RBRACE
+    | T_PRESERVING -> PRESERVING
+    | T_PLUS -> PLUS ""
+    | T_PERM -> PERM
+    | T_PACK -> PACK
+    | T_OPPREFIX -> OPPREFIX ""
+    | T_OPINFIX4 -> OPINFIX4 ""
+    | T_OPINFIX3 -> OPINFIX3 ""
+    | T_OPINFIX2 -> OPINFIX2 ""
+    | T_OPINFIX1 -> OPINFIX1 ""
+    | T_OPINFIX0d -> OPINFIX0d ""
+    | T_OPINFIX0c -> OPINFIX0c ""
+    | T_OPINFIX0b -> OPINFIX0b ""
+    | T_OPINFIX0a -> OPINFIX0a ""
+    | T_OPEN -> OPEN
+    | T_MUTABLE -> MUTABLE
+    | T_MINUS -> MINUS ""
+    | T_MATCH -> MATCH
+    | T_LPAREN -> LPAREN
+    | T_LIDENT -> LIDENT ""
+    | T_LET -> LET
+    | T_LBRACKET -> LBRACKET
+    | T_LBRACE -> LBRACE
+    | T_LARROW -> LARROW
+    | T_INT -> INT 0
+    | T_IN -> IN
+    | T_IF -> IF
+    | T_GIVE -> GIVE
+    | T_FUN -> FUN
+    | T_FROM -> FROM
+    | T_FOR -> FOR
+    | T_FLEX -> FLEX
+    | T_FAIL -> FAIL
+    | T_FACT -> FACT
+    | T_EXPLAIN -> EXPLAIN
+    | T_EXCLUSIVE -> EXCLUSIVE
+    | T_EQUAL -> EQUAL ""
+    | T_EOF -> EOF
+    | T_END -> END
+    | T_EMPTY -> EMPTY
+    | T_ELSE -> ELSE
+    | T_DYNAMIC -> DYNAMIC
+    | T_DUPLICABLE -> DUPLICABLE
+    | T_DOWNTO -> DOWNTO
+    | T_DOT -> DOT
+    | T_DO -> DO
+    | T_DBLARROW -> DBLARROW
+    | T_DATA -> DATA
+    | T_CONSUMES -> CONSUMES
+    | T_COMMA -> COMMA
+    | T_COLONEQUAL -> COLONEQUAL ""
+    | T_COLONCOLON -> COLONCOLON
+    | T_COLON -> COLON
+    | T_BUILTIN -> BUILTIN
+    | T_BELOW -> BELOW
+    | T_BEGIN -> BEGIN
+    | T_BAR -> BAR
+    | T_AT -> AT
+    | T_ASSERT -> ASSERT
+    | T_AS -> AS
+    | T_ARROW -> ARROW
+    | T_AND -> AND
+    | T_ALIAS -> ALIAS
+    | T_ADOPTS -> ADOPTS
+    | T_ABSTRACT -> ABSTRACT
+    | T_ABOVE -> ABOVE
+
+end
+
 (* Lexing and parsing, built on top of [grammar]. *)
 
-let lex_and_parse_raw lexbuf file_path entry_var =
-  let the_parser = MenhirLib.Convert.Simplified.traditional2revised entry_var in
+module P =
+  MenhirLib.Printers.Make
+    (Grammar.MenhirInterpreter)
+    (struct
+      let print s = Printf.fprintf stderr "%s" s
+      let print_symbol s = print (PS.print_symbol s)
+      let print_element = None
+     end)
+
+module E =
+  MenhirLib.ErrorReporting.Make
+    (Grammar.MenhirInterpreter)
+    (TT)
+
+let lex_and_parse_raw lexbuf file_path (entry : unit -> 'a Grammar.MenhirInterpreter.result) : 'a =
+  let the_parser = E.entry (entry()) in
+  let the_parser = MenhirLib.Convert.Simplified.traditional2revised the_parser in
   try
     Lexer.init file_path;
     the_parser (fun _ -> Lexer.token lexbuf)
@@ -47,9 +360,10 @@ let lex_and_parse_raw lexbuf file_path entry_var =
         Printf.eprintf
           "Invalid code var %i at offset %i\n" i (Ulexing.lexeme_end lexbuf);
         exit 254
-    | Grammar.Error ->
+    | E.Error explanations ->
         MzString.beprintf "%a\nError: Syntax error\n"
           print_position lexbuf;
+        List.iter P.print_item explanations;
         let start_pos = Lexer.start_pos lexbuf in
         let end_pos = Lexer.end_pos lexbuf in
         JsGlue.highlight_range start_pos end_pos;
@@ -147,11 +461,11 @@ let mkprefix path =
 ;;
 
 let lex_and_parse_implementation path : SurfaceSyntax.implementation =
-  mkprefix path @ lex_and_parse path Grammar.implementation
+  mkprefix path @ lex_and_parse path Grammar.Incremental.implementation
 ;;
 
 let lex_and_parse_interface path : SurfaceSyntax.interface =
-  mkprefix path @ lex_and_parse path Grammar.interface
+  mkprefix path @ lex_and_parse path Grammar.Incremental.interface
 ;;
 
 
